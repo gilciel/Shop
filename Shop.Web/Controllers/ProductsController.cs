27993075d3dd789c29766aeba3c80b8a5bc68f37
gilciel <gilcielruby@gmail.com>
2019-02-15 23:11:28 -0200

    Generic Repository e Add User

--- Shop.Web/Controllers/ProductsController.cs
+++ Shop.Web/Controllers/ProductsController.cs
@@ -1,39 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Shop.Web.Data;
-using Shop.Web.Data.Entities;
-
-namespace Shop.Web.Controllers
+﻿namespace Shop.Web.Controllers
 {
+    using System.Threading.Tasks;
+    using Data;
+    using Data.Entities;
+    using Helpers;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+
     public class ProductsController : Controller
     {
-        private readonly IRepository repository;
+        private readonly IProductRepository productRepository;
 
-        public ProductsController(IRepository repository)
+        private readonly IUserHelper userHelper;
+
+        public ProductsController(IProductRepository productRepository, IUserHelper userHelper)
         {
-            this.repository = repository;
+            this.productRepository = productRepository;
+            this.userHelper = userHelper;
         }
 
         // GET: Products
         public IActionResult Index()
         {
-            return View(this.repository.GetProducts());
+            return View(this.productRepository.GetAll());
         }
 
         // GET: Products/Details/5
-        public IActionResult Details(int? id)
+        public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var product = this.repository.GetProduct(id.Value);
+            var product = await this.productRepository.GetByIdAsync(id.Value);
             if (product == null)
             {
                 return NotFound();
@@ -55,26 +55,29 @@ namespace Shop.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                this.repository.AddProduct(product);
-                await this.repository.SaveAllAsync();
+                // TODO: Pending to change to: this.User.Identity.Name
+                product.User = await this.userHelper.GetUserByEmailAsync("jzuluaga55@gmail.com");
+                await this.productRepository.CreateAsync(product);
                 return RedirectToAction(nameof(Index));
             }
+
             return View(product);
         }
 
         // GET: Products/Edit/5
-        public IActionResult Edit(int? id)
+        public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var product = this.repository.GetProduct(id.Value);
+            var product = await this.productRepository.GetByIdAsync(id.Value);
             if (product == null)
             {
                 return NotFound();
             }
+
             return View(product);
         }
 
@@ -87,12 +90,13 @@ namespace Shop.Web.Controllers
             {
                 try
                 {
-                    this.repository.UpdateProduct(product);
-                    await this.repository.SaveAllAsync();
+                    // TODO: Pending to change to: this.User.Identity.Name
+                    product.User = await this.userHelper.GetUserByEmailAsync("jzuluaga55@gmail.com");
+                    await this.productRepository.UpdateAsync(product);
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!this.repository.ProductExists(product.Id))
+                    if (!await this.productRepository.ExistAsync(product.Id))
                     {
                         return NotFound();
                     }
@@ -103,18 +107,19 @@ namespace Shop.Web.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
+
             return View(product);
         }
 
         // GET: Products/Delete/5
-        public IActionResult Delete(int? id)
+        public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var product = this.repository.GetProduct(id.Value);
+            var product = await this.productRepository.GetByIdAsync(id.Value);
             if (product == null)
             {
                 return NotFound();
@@ -128,10 +133,147 @@ namespace Shop.Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var product = this.repository.GetProduct(id);
-            this.repository.RemoveProduct(product);
-            await this.repository.SaveAllAsync();
+            var product = await this.productRepository.GetByIdAsync(id);
+            await this.productRepository.DeleteAsync(product);
             return RedirectToAction(nameof(Index));
         }
     }
+
+    //using Data;
+    //using Data.Entities;
+    //using Helpers;
+    //using Microsoft.AspNetCore.Mvc;
+    //using Microsoft.EntityFrameworkCore;
+    //using System.Threading.Tasks;
+
+    //public class ProductsController : Controller
+    //{
+    //    private readonly IRepository repository;
+    //    private readonly IUserHelper userHelper;
+
+    //    public ProductsController(IRepository repository, IUserHelper userHelper)
+    //    {
+    //        this.repository = repository;
+    //        this.userHelper = userHelper;
+    //    }
+
+    //    // GET: Products
+    //    public IActionResult Index()
+    //    {
+    //        return View(this.repository.GetProducts());
+    //    }
+
+    //    // GET: Products/Details/5
+    //    public IActionResult Details(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        var product = this.repository.GetProduct(id.Value);
+    //        if (product == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        return View(product);
+    //    }
+
+    //    // GET: Products/Create
+    //    public IActionResult Create()
+    //    {
+    //        return View();
+    //    }
+
+    //    // POST: Products/Create
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public async Task<IActionResult> Create(Product product)
+    //    {
+    //        if (ModelState.IsValid)
+    //        {
+    //            //TODO:Change for the logged user
+    //            product.User = await this.userHelper.GetUserByEmailAsync("gilcielphp@gmail.com");
+    //            this.repository.AddProduct(product);
+    //            await this.repository.SaveAllAsync();
+    //            return RedirectToAction(nameof(Index));
+    //        }
+    //        return View(product);
+    //    }
+
+    //    // GET: Products/Edit/5
+    //    public IActionResult Edit(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        var product = this.repository.GetProduct(id.Value);
+    //        if (product == null)
+    //        {
+    //            return NotFound();
+    //        }
+    //        return View(product);
+    //    }
+
+    //    // POST: Products/Edit/5
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public async Task<IActionResult> Edit(Product product)
+    //    {
+    //        if (ModelState.IsValid)
+    //        {
+    //            try
+    //            {
+    //                //TODO:Change for the logged user
+    //                product.User = await this.userHelper.GetUserByEmailAsync("gilcielphp@gmail.com");
+    //                this.repository.UpdateProduct(product);
+    //                await this.repository.SaveAllAsync();
+    //            }
+    //            catch (DbUpdateConcurrencyException)
+    //            {
+    //                if (!this.repository.ProductExists(product.Id))
+    //                {
+    //                    return NotFound();
+    //                }
+    //                else
+    //                {
+    //                    throw;
+    //                }
+    //            }
+    //            return RedirectToAction(nameof(Index));
+    //        }
+    //        return View(product);
+    //    }
+
+    //    // GET: Products/Delete/5
+    //    public IActionResult Delete(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        var product = this.repository.GetProduct(id.Value);
+    //        if (product == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        return View(product);
+    //    }
+
+    //    // POST: Products/Delete/5
+    //    [HttpPost, ActionName("Delete")]
+    //    [ValidateAntiForgeryToken]
+    //    public async Task<IActionResult> DeleteConfirmed(int id)
+    //    {
+    //        var product = this.repository.GetProduct(id);
+    //        this.repository.RemoveProduct(product);
+    //        await this.repository.SaveAllAsync();
+    //        return RedirectToAction(nameof(Index));
+    //    }
+    //}
 }
--- Shop.Web/Data/SeedDb.cs
+++ Shop.Web/Data/SeedDb.cs
@@ -5,25 +5,27 @@
     using System.Threading.Tasks;
     using Entities;
     using Microsoft.AspNetCore.Identity;
+    using Shop.Web.Helpers;
 
     public class SeedDb
     {
         private readonly DataContext context;
-        private readonly UserManager<User> userManager;
-
+        private readonly IUserHelper userHelper;
         private Random random;
 
-        public SeedDb(DataContext context,UserManager<User> userManager)
+        public SeedDb(DataContext context, IUserHelper userHelper)
         {
             this.context = context;
-            this.userManager = userManager;
+            this.userHelper = userHelper;
             this.random = new Random();
         }
 
         public async Task SeedAsync()
         {
             await this.context.Database.EnsureCreatedAsync();
-            var user = await this.userManager.FindByEmailAsync("gilcielphp@gmail.com");
+
+            // Add user
+            var user = await this.userHelper.GetUserByEmailAsync("gilcielphp@gmail.com");
             if (user == null)
             {
                 user = new User
@@ -34,24 +36,24 @@
                     UserName = "gilcielphp@gmail.com"
                 };
 
-                var result = await this.userManager.CreateAsync(user, "123456");
+                var result = await this.userHelper.AddUserAsync(user, "123456");
                 if (result != IdentityResult.Success)
                 {
                     throw new InvalidOperationException("Could not create the user in seeder");
                 }
             }
 
-
+            // Add products
             if (!this.context.Products.Any())
             {
                 this.AddProduct("iPhone X", user);
-                this.AddProduct("Maginc Mouse", user);
+                this.AddProduct("Magic Mouse", user);
                 this.AddProduct("iWatch Series 4", user);
                 await this.context.SaveChangesAsync();
             }
         }
 
-        private void AddProduct(string name,User user)
+        private void AddProduct(string name, User user)
         {
             this.context.Products.Add(new Product
             {
@@ -63,4 +65,68 @@
             });
         }
     }
+
+    //using System;
+    //using System.Linq;
+    //using System.Threading.Tasks;
+    //using Entities;
+    //using Microsoft.AspNetCore.Identity;
+    //using Helpers;
+
+    //public class SeedDb
+    //{
+    //    private readonly DataContext context;
+    //    private readonly IUserHelper userHelper;
+    //    private Random random;
+
+    //    public SeedDb(DataContext context,IUserHelper userHelper)
+    //    {
+    //        this.context = context;
+    //        this.userHelper = userHelper;
+    //        this.random = new Random();
+    //    }
+
+    //    public async Task SeedAsync()
+    //    {
+    //        await this.context.Database.EnsureCreatedAsync();
+    //        var user = await this.userHelper.GetUserByEmailAsync("gilcielphp@gmail.com");
+    //        if (user == null)
+    //        {
+    //            user = new User
+    //            {
+    //                FirstName = "Gilciel",
+    //                LastName = "Almeida",
+    //                Email = "gilcielphp@gmail.com",
+    //                UserName = "gilcielphp@gmail.com"
+    //            };
+
+    //            var result = await this.userHelper.AddUserAsync(user, "123456");
+    //            if (result != IdentityResult.Success)
+    //            {
+    //                throw new InvalidOperationException("Could not create the user in seeder");
+    //            }
+    //        }
+
+
+    //        if (!this.context.Products.Any())
+    //        {
+    //            this.AddProduct("iPhone X", user);
+    //            this.AddProduct("Maginc Mouse", user);
+    //            this.AddProduct("iWatch Series 4", user);
+    //            await this.context.SaveChangesAsync();
+    //        }
+    //    }
+
+    //    private void AddProduct(string name,User user)
+    //    {
+    //        this.context.Products.Add(new Product
+    //        {
+    //            Name = name,
+    //            Price = this.random.Next(1000),
+    //            IsAvailabe = true,
+    //            Stock = this.random.Next(100),
+    //            User = user
+    //        });
+    //    }
+    //}
 }
