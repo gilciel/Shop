f430874d617a6ffc0c31768fc865b540d432ee91
gilciel <gilcielruby@gmail.com>
2019-03-26 21:50:36 -0300

    Recovery Password

--- Shop.Web/Helpers/IUserHelper.cs
+++ Shop.Web/Helpers/IUserHelper.cs
@@ -20,5 +20,8 @@
         Task<string> GenerateEmailConfirmationTokenAsync(User user);
         Task<IdentityResult> ConfirmEmailAsync(User user, string token);
         Task<User> GetUserByIdAsync(string userId);
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
+
     }
 }
---
+++ Shop.Web/Models/RecoverPasswordViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop.Web.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class RecoverPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
