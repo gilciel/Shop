79619f9fe8b27f97558a8a2a706dc4b3568f092d
gilciel <gilcielruby@gmail.com>
2019-04-17 21:41:30 -0300

    New Methods On API

--- Shop.Web/Data/Repositories/IGenericRepository.cs
+++ Shop.Web/Data/Repositories/IGenericRepository.cs
@@ -9,12 +9,12 @@
 
         Task<T> GetByIdAsync(int id);
 
-        Task CreateAsync(T entity);
-
-        Task UpdateAsync(T entity);
-
         Task DeleteAsync(T entity);
 
         Task<bool> ExistAsync(int id);
+
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+
     }
 }
