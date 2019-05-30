a3e8f56775f5b429022d620cd817fbc5f6aa6dcf
gilciel <gilcielruby@gmail.com>
2019-05-29 22:42:15 -0300

    Splash on Android e Application Icon

--- Shop.UIForms/Shop.UIForms.Android/MainActivity.cs
+++ Shop.UIForms/Shop.UIForms.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Plugin.Permissions;
 
 namespace Shop.UIForms.Droid
 {
-    [Activity(Label = "Shop", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Shop", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
