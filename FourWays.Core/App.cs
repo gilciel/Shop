eee2fec3baa7a94b50b801c00b92f02b57106780
gilciel <gilcielruby@gmail.com>
2019-06-18 23:22:53 -0300

    MVVM CROSS DROID

---
+++ FourWays.Core/App.cs
@@ -0,0 +1,20 @@
+﻿namespace FourWays.Core
+{
+    using ViewsModels;
+    using MvvmCross.IoC;
+    using MvvmCross.ViewModels;
+
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            this.CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            this.RegisterAppStart<TipViewModel>();
+        }
+    }
+
+}
---
+++ FourWays.Cross.Droid/Views/TipView.cs
@@ -0,0 +1,18 @@
+﻿namespace FourWays.Cross.Droid.Views
+{
+    using Android.App;
+    using Android.OS;
+    using FourWays.Core.ViewsModels;
+    using MvvmCross.Platforms.Android.Views;
+
+    [Activity(Label = "@string/app_name")]
+    public class TipView : MvxActivity<TipViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            this.SetContentView(Resource.Layout.TipPage);
+        }
+    }
+
+}
